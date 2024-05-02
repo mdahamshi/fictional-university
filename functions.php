c9274c7cad3be10e022e6b3dbdd872fefb791cb7
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2024-05-02 18:14:12 +0300

    fixed warning in new php

--- functions.php
+++ functions.php
@@ -12,15 +12,15 @@ add_action('rest_api_init', 'university_custom_rest');
 
 function pageBanner($args = NULL) {
   
-  if (!$args['title']) {
+  if (!$args || !array_key_exists('title', $args)) {
     $args['title'] = get_the_title();
   }
 
-  if (!$args['subtitle']) {
+  if (!$args || !array_key_exists('title', $args)) {
     $args['subtitle'] = get_field('page_banner_subtitle');
   }
 
-  if (!$args['photo']) {
+  if (!$args || !array_key_exists('title', $args)) {
     if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
       $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
     } else {
